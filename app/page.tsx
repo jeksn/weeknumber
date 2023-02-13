e7a60198fe1e093dac56339b748f5b8d2f708456
Johan <johan@wtmg.se>
2023-02-13 16:53:33 +0100

    make page dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import dayjs from 'dayjs'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import MyDialog from './components/Dialog'
 
+export const dynamic = 'force-dynamic'
+
 function WeekNum() {
   dayjs.extend(weekOfYear);
   const WeekNumber = dayjs().week(); 
+  const Second = dayjs().second(); 
   return (
     <>
-    <div className="flex h-[95vh] justify-center items-center">
+    <div className="flex flex-col h-[95vh] justify-center items-center">
       <h2 className="text-white text-[10rem]">{WeekNumber}</h2>
+      <h2 className="text-white text-[10rem]">{Second}</h2>
     </div>
     </>
   )
@@ -18,7 +22,7 @@ export default function Home() {
   return (
     <main>
       <MyDialog />
-      <WeekNum />
+        <WeekNum />
     </main>
   )
 }
