dbd74f8ceb7ada4ad948ba860ee6fe95f5b51e22
Johan <johan@wtmg.se>
2023-02-13 16:55:55 +0100

    remove seconds test

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,10 @@ export const dynamic = 'force-dynamic'
 function WeekNum() {
   dayjs.extend(weekOfYear);
   const WeekNumber = dayjs().week(); 
-  const Second = dayjs().second(); 
   return (
     <>
     <div className="flex flex-col h-[95vh] justify-center items-center">
       <h2 className="text-white text-[10rem]">{WeekNumber}</h2>
-      <h2 className="text-white text-[10rem]">{Second}</h2>
     </div>
     </>
   )
@@ -22,7 +20,7 @@ export default function Home() {
   return (
     <main>
       <MyDialog />
-        <WeekNum />
+      <WeekNum />
     </main>
   )
 }
